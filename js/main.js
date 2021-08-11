040eeb24b34d445d0ce4df91accec4aedb1159ec
felipe-souza17 <felipesantos.rsouza@gmail.com>
2021-08-11 11:27:25 -0300

    Página inicial

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+    var request = new XMLHttpRequest()
+
+    request.open('GET', 'https://api.adviceslip.com/advice')
+    
+    request.onload = function () {
+        var data = JSON.parse(this.response)
+    
+        console.log(data)
+        console.log(data.slip.advice)
+        console.log(data.slip.id)
+
+        // document.getElementById("clicou").innerHTML = data.slip.advice
+    }
+    
+    request.send()
+
