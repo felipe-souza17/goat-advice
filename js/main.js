9c9098f6bbad85109e383e370e755a9b87f960be
felipe-souza17 <felipesantos.rsouza@gmail.com>
2022-03-10 14:57:27 -0300

    Change to fetch

--- js/main.js
+++ js/main.js
@@ -1,20 +1,8 @@
-
-    var request = new XMLHttpRequest()
-
-    request.open('GET', 'https://api.adviceslip.com/advice')
-    
-    request.onload = function () {
-        var data = JSON.parse(this.response)
-    
-        console.log(data)
-        console.log(data.slip.advice)
-        console.log(data.slip.id)
-
-        document.getElementById("advice").innerHTML = '"' + data.slip.advice + '"'
-
-        
-        // document.getElementById("clicou").innerHTML = data.slip.advice
-    }
-    
-    request.send()
-
+let url = 'https://api.adviceslip.com/advice'
+fetch(url)
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    document.getElementById('advice').innerHTML = '"' + data.slip.advice + '"'
+  })
